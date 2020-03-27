9a63097dc77e3436fdf605111fdce581f304b37b
Aniket Biprojit Chowdhury <aniket_chowdhury@hotmail.com>
2020-03-27 10:01:32 +0530

    Https redirects in non-devs

--- server/index.js
+++ server/index.js
@@ -20,23 +20,35 @@ try {
 // React development server runs on port 3000
 // We wil stick to 8080 port for express server
 const port = process.env.PORT
+const https_port = process.env.HTTPS_PORT
 
 const app = express()
-
-// Express Configurations
-// process.env.NODE_ENV === 'development'
-// 	? app.use(morgan('dev'))
-// 	: app.use(
-// 			morgan('common', {
-// 				stream: __dirname + '/../morgan.log'
-// 			})
-// 	  )
 
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+// https redirect in production and build
 app.enable('trust proxy')
-app.use(require(__dirname+'/middlewares/httpsRedirect.js'))
-app.use('/api',require(__dirname+'/routes/api'))
-app.listen(port, () => console.log(`Listening on port ${port}`))
+if (process.env.NODE_ENV != 'dev') {
+	app.use(require(__dirname + '/middlewares/httpsRedirect.js'))
+}
+
+app.use('/api', require(__dirname + '/routes/api'))
+
+const https = require('https')
+
+const fs = require('fs')
+const options = {
+	key: fs.readFileSync(__dirname + '/./server.key'),
+	cert: fs.readFileSync(__dirname + '/./server.crt')
+}
+
+app.listen(port, () => console.log(`App listening on port ${port}`))
+
+
+https
+	.createServer(options, app)
+	.listen(https_port, () =>
+		console.log(`Https listening on port ${https_port}`)
+	)
